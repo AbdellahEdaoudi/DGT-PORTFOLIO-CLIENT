35eb745a5dc67b3d86d93021cb99d36724eee600
abdellah <abdellahedaoudi80@gmail.com>
2025-12-05 02:30:00 +0100

    feat: Add dynamic user portfolio page with metadata generation.

--- app/[username]/page.jsx
+++ app/[username]/page.jsx
@@ -7,9 +7,7 @@ export async function generateMetadata({ params }) {
   const backendUrl = process.env.BACKEND_URL || 'https://dgt-portfolio-server.vercel.app';
   // 3️⃣ Fetch User Metadata
   if (params.username) {
-    const res = await fetch(`${backendUrl}/users/metauser/${params.username}`, {
-      cache: 'no-store',
-    });
+    const res = await fetch(`${backendUrl}/users/metauser/${params.username}`);
     if (res.status === 404) notFound();
     const data = await res.json();
     if (data.status && data.user) {
